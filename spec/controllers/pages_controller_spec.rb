8786a971251c61fea0e99f68b83d865b229f354d
mt-sagarmatha <eminisrafil@yahoo.com>
2012-10-03 20:50:53 -0400

    stylesheets and refactor

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 
   render_views
+  
+  before(:each) do
+  	@base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -13,7 +17,7 @@ describe PagesController do
     it "should have the right title" do
     	get 'home'
     	response.should have_selector("title", 
-    	:content => "Ruby on Rails Tutorial Sample App | Home")
+    	:content => "#{@base_title} | Home")
     end
     
     it "shout have a non blank body" do
