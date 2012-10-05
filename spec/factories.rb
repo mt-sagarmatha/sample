e25d4335e70571d404c6852f1d487e0c9ebe927b
mt-sagarmatha <eminisrafil@yahoo.com>
2012-10-05 00:38:08 -0400

    added user factories +

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name                  "emin"
+	user.email                 "emin@gmail.com"
+	user.password              "foofoobar"
+	user.password_confirmation "foofoobar"
+end
