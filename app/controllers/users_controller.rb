192a5c23164351a778cf8a630788d1f65b9a18b5
mt-sagarmatha <eminisrafil@yahoo.com>
2012-10-08 17:13:45 -0400

    delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController 
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
 
   def index
     @users = User.paginate(:page => params[:page])
@@ -46,6 +47,11 @@ class UsersController < ApplicationController
     end     
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success => "deleted yo"]
+  end
+
 private
   def authenticate
        deny_access unless signed_in? 
@@ -54,7 +60,10 @@ private
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
-    
+  end
+
+  def admin_user
+    redirect_to(root_path) unless (current_user.admin? && !current_user?(user))    
   end
  
   
